d1ddb3856cbce7f72b5f16ad81cbcec47931a07c
Tales Luna <tales.ferreira.luna@gmail.com>
2017-02-16 03:13:25 -0200

    RastroJS v1.0.2, configs e main script

--- configs/config.js
+++ configs/config.js
@@ -3,16 +3,13 @@
     @author     Tales Luna <tale.ferreira.luna@gmail.com>
 
     Configurações básicas da API
-
  */
 var configs = function() {
-
     return  {
-
         app_port    : '3000',
         app_host    : '127.0.0.1',
         app_name    : 'Rastro - API Rastreamento de objetos - Correios',
-        app_version : 'v1',
+        app_version : 'v1.0.2',
         app_origin  : '*'
     };
 };
